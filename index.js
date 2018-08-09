1639c55452ae07ec8a1262c7244e910bdbd7e786
StevenAprentisCodeur <41762297+StevenAprentisCodeur@users.noreply.github.com>
2018-08-09 17:41:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ bot.login(process.env.TOKEN);
 bot.on('message', message => {
     if (message.content === prefix + "help"){
         message.channel.sendMessage("**__Aide:__** \n -__help:__ afficher ce message \n -__youtube:__ afficher la chaîne youtube de NeoflasH");
-    }
+    }  
+  });
 bot.on('message', message => {
     if (message.content === prefix + "youtube"){
         message.channel.sendMessage("Chaîne Youtube de NeoflasH : https://www.youtube.com/channel/UCDBIzgcBjtlGjnj6jkbZsnA?");
